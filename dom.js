3ef184f10c61108799243406d8042e3eded836da
fatima-harun <faabintu@gmail.com>
2023-05-04 13:30:58 +0000

    rectification des boutons

--- dom.js
+++ dom.js
@@ -1,42 +1,56 @@
-  const items ={
-	apple:{price:1000 , quantity:0 },
-	banana:{price:2000 ,quantity:0},
-	orange:{price:2000 ,quantity:0},
-  };
- 
-  function adjustQuantity(item, change){
+const items = {
+	apple: { price: 1000, quantity: 0 },
+	banana: { price: 2000, quantity: 0 },
+	orange: { price: 2000, quantity: 0 },
+};
+
+function adjustQuantity(item, change) {
 	items[item].quantity += change;
-	if (items[item].quantity < 0){
+	if (items[item].quantity < 0) {
 		items[item].quantity = 0;
 	}
 	updateCart();
 }
-function updateCart(){
+function updateCart() {
 	document.getElementById("apple-quantity").textContent =
-	items.apple.quantity;
+		items.apple.quantity;
 	document.getElementById("banana-quantity").textContent =
-	items.banana.quantity;
+		items.banana.quantity;
 	document.getElementById("orange-quantity").textContent =
-	items.orange.quantity;
+		items.orange.quantity;
 
-let total = 0;
-for (let item in items) {
-	total += items[item].price * items[item].quantity;
-   }
- document.getElementById("total-price").textContent = 
- "prix-total: FCFA" + total;
+	let total = 0;
+	for (let item in items) {
+		total += items[item].price * items[item].quantity;
+	}
+	document.getElementById("total-price").textContent = total;
 }
 
-function changerCouleur(element){
+function changerCouleur(element) {
 	if (element.style.backgroundColor == 'red')
-	element.style.backgroundColor ='white';
+		element.style.backgroundColor = 'white';
 	else
-	element.style.backgroundColor = 'red';
+		element.style.backgroundColor = 'red';
 }
 
-function supprimer(item){
-	items[item].price ;
-	if (items[item].quantity < 0){
+function supprimer(item) {
+	let prix = items[item].price;
+	let quantite = items[item].quantity;
+	let ancienTotal = document.getElementById("total-price").innerText;
+	let total = ancienTotal - (prix * quantite);
+
+	document.getElementById("total-price").textContent = total;
+
+	if (item == 'apple') {
+		document.getElementById("apple-quantity").textContent = 0;
+		items[item].quantity = 0;
+	}
+	else if (item == 'banana') {
+		document.getElementById("banana-quantity").textContent = 0;
+		items[item].quantity = 0;
+	}
+	else {
+		document.getElementById("orange-quantity").textContent = 0;
 		items[item].quantity = 0;
 	}
 }
