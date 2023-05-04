57d36e79e65011ea8a5c743a4815597d4daa0770
fatima-harun <faabintu@gmail.com>
2023-05-04 12:22:19 +0000

    ajout du bouton j'aime et supp

--- dom.js
+++ dom.js
@@ -1,52 +1,42 @@
-// Récupérer tous les boutons "Ajouter au panier"
-var boutonsAjouter = document.querySelectorAll('.ajouter-au-panier');
+  const items ={
+	apple:{price:1000 , quantity:0 },
+	banana:{price:2000 ,quantity:0},
+	orange:{price:2000 ,quantity:0},
+  };
+ 
+  function adjustQuantity(item, change){
+	items[item].quantity += change;
+	if (items[item].quantity < 0){
+		items[item].quantity = 0;
+	}
+	updateCart();
+}
+function updateCart(){
+	document.getElementById("apple-quantity").textContent =
+	items.apple.quantity;
+	document.getElementById("banana-quantity").textContent =
+	items.banana.quantity;
+	document.getElementById("orange-quantity").textContent =
+	items.orange.quantity;
 
-// Ajouter un écouteur d'événements pour chaque bouton "Ajouter au panier"
-boutonsAjouter.forEach(function(bouton) {
-  bouton.addEventListener('click', function() {
-    // Récupérer le nom et le prix de l'article à ajouter
-    var article = this.closest('.article');
-    var nomArticle = article.querySelector('h2').innerText;
-    var prixAritcle = article.querySelector('p').innerText;
-
-    // Appeler la fonction ajouterAuPanier
-    ajouterAuPanier(nomArticle, prixAritcle);
-  });
-});
-
-// Définir la fonction ajouterAuPanier
-function ajouterAuPanier(nom, prix) {
-  // Récupérer l'élément qui affiche le nombre d'articles dans le panier
-  var nombreArticles = document.querySelector('.nombre-articles-panier');
-
-  // Récupérer le nombre actuel d'articles dans le panier
-  var nombreArticlesActuel = parseInt(nombreArticles.innerText);
-
-  // Ajouter 1 au nombre d'articles dans le panier
-  var nouveauNombreArticles = nombreArticlesActuel + 1;
-
-  // Mettre à jour le nombre d'articles dans l'icône de panier
-  nombreArticles.innerText = nouveauNombreArticles;
-
-  // Afficher un message de confirmation
-  alert("L'article " + nom + " a été ajouté au panier.");
+let total = 0;
+for (let item in items) {
+	total += items[item].price * items[item].quantity;
+   }
+ document.getElementById("total-price").textContent = 
+ "prix-total: FCFA" + total;
 }
 
-const minusBtns = document.querySelectorAll('.minus');
-  const plusBtns = document.querySelectorAll('.plus');
-  const qtyInputs = document.querySelectorAll('.num');
+function changerCouleur(element){
+	if (element.style.backgroundColor == 'red')
+	element.style.backgroundColor ='white';
+	else
+	element.style.backgroundColor = 'red';
+}
 
-  minus.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
-      if (qtyInputs[i] > 0) {
-        qtyInputs[i]--;
-      }
-    });
-  });
-
-  plus.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
-      qtyInputs[i]++;
-    });
-  });
-
+function supprimer(item){
+	items[item].price ;
+	if (items[item].quantity < 0){
+		items[item].quantity = 0;
+	}
+}
